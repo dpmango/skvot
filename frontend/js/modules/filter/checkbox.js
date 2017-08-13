131d805df2f8990746f662ae03c63bf199ca995b
Sergey Khmelevskoy <xs290@me.com>
2017-08-13 15:20:34 +0300

    checkbox number

--- frontend/js/modules/filter/checkbox.js
+++ frontend/js/modules/filter/checkbox.js
@@ -4,12 +4,16 @@ export let checkbox = {
   filterCheckbox: '.filter-checkbox',
   showAll: '.filter-checkbox__all',
   checkbox: '.filter-checkbox__box',
+  checkboxInput: '.filter-checkbox__box input',
   init () {
     let self = this;
     $(this.showAll).on('click', function(e) {
       e.preventDefault();
       self.openCheckboxes(self, this);
     });
+    $(this.checkboxInput).on('change', function(e){
+      self.showGoodsNumber(self, this);
+    })
   },
   openCheckboxes(vm, element) {
       let checkboxes = $(element).closest(vm.filterCheckbox).find(vm.checkbox + ':hidden').toArray();
@@ -23,6 +27,14 @@ export let checkbox = {
           return i * 50;
         }
       })
+  },
+  showGoodsNumber(vm, element) {
+    $('.filter-aside__number').remove();
+    let numberElement = "<div class='filter-aside__number'>Показать 67 товаров</div>";
+    $(element).parent().append(numberElement);
+    setTimeout(function(){
+      $(element).parent().find('.filter-aside__number').addClass('visible');
+    },100);
   }
 }
 
